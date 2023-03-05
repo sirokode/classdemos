6a57baac7c323e44d2e22cb021dcd407dfd1c5f1
sirokode <112339757+sirokode@users.noreply.github.com>
2023-03-05 12:30:04 -0600

    Update install.sh

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,3 +1,6 @@
 sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
+
+#
+apt-get update && apt install python3-pip && apt install awscli && pip install boto
