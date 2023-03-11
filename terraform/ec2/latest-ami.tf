9c464a63906a67a73706f44635edc79466209c01
sirokode <112339757+sirokode@users.noreply.github.com>
2023-03-11 15:57:56 -0600

    latest ami find

---
+++ terraform/ec2/latest-ami.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+output "test" {
+  value = data.aws_ami.ubuntu.id
+}
