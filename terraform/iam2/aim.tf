c11840a0a6facb158e6ae78902e81e64d41efbdb
sirokode <112339757+sirokode@users.noreply.github.com>
2023-03-11 20:47:57 -0600

    Update aim.tf

--- terraform/iam2/aim.tf
+++ terraform/iam2/aim.tf
@@ -3,6 +3,11 @@ resource "aws_iam_user" "lb" {
   name = "z_test_user"
 }
 
+#Uses PGP to encrypt the password for safe transport to the user. 
+#PGP keys can be obtained from Keybase
+/* for example, use mcowiti has a pgp public key at keybase.io, they can use that to
+  decrypt the password and or secret key generated 
+  */
 resource "aws_iam_user_login_profile" "u" {
   user  = "${aws_iam_user.lb.name}"
   password_reset_required = true
