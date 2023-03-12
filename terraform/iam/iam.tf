65cc0e1866448ccb1c2442788d235186c8276b99
sirokode <112339757+sirokode@users.noreply.github.com>
2023-03-11 20:51:32 -0600

    Update iam.tf

--- terraform/iam/iam.tf
+++ terraform/iam/iam.tf
@@ -2,6 +2,7 @@ resource "aws_iam_user" "userexample" {
   name          = "tf-test-user"
   path          = "/"
   force_destroy = true
+  #a user with non-Terraform-managed access keys and login profile will fail to be destroyed otherwise during destroy
 }
 
 resource "aws_iam_user_login_profile" "example" {
