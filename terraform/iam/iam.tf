b3ca20837e507d1d655623d0560922a6cb6a10f5
sirokode <112339757+sirokode@users.noreply.github.com>
2023-03-11 20:47:32 -0600

    simple IAM creation

--- terraform/iam/iam.tf
+++ terraform/iam/iam.tf
@@ -4,11 +4,7 @@ resource "aws_iam_user" "userexample" {
   force_destroy = true
 }
 
-#Uses PGP to encrypt the password for safe transport to the user. 
-#PGP keys can be obtained from Keybase
-/* for example, use mcowiti has a pgp public key at keybase.io, they can use that to
-  decrypt the password and or secret key generated 
-  */
+
 resource "aws_iam_user_login_profile" "example" {
   user = aws_iam_user.userexample.name
   #pgp_key = "keybase:mcowiti"
