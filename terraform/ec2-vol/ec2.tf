ab079a2c6da9d2f5d4be2c15041f215b15dea988
sirokode <112339757+sirokode@users.noreply.github.com>
2023-03-18 16:45:08 -0500

    ec2 with volume attachment

--- terraform/ec2-vol/ec2.tf
+++ terraform/ec2-vol/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami               = "ami-21f78e11"
+  ami               = "ami-put-correct-ami-here"
   availability_zone = "us-east-1a"
   instance_type     = "t2.micro"
 
@@ -7,4 +7,10 @@ resource "aws_instance" "web" {
     Name = "Tf-TestEc2-with-Vol"
     usage ="evaluations"
   }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
 }
